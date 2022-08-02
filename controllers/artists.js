03e89de941b9a8b5cf3b7bb40dcff5fe7d82e0b7
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2022-08-02 23:44:35 +0700

    menambahkan logic API untuk api/artists dan api/artists/:id

--- controllers/artists.js
+++ controllers/artists.js
@@ -4,32 +4,112 @@ const db = new sqlite3.Database(
 );
 
 const getAllArtists = async (req, res) => {
-   await db.all(`SELECT * FROM Artist`, (err, data) => {
-      err ? res.status(404).send("404 Not Found") : res.status(200).json(data);
-   });
+   try {
+      await db.all(`SELECT * FROM Artist`, (err, data) => {
+         err
+            ? res.status(404).send("404 Not Found")
+            : res.status(200).json(data);
+      });
+   } catch (error) {
+      res.status(500).send(error);
+   }
 };
 
 const createNewArtists = async (req, res) => {
    const { artist } = req.body;
 
-   // console.log(data);
-   await db.run(
-      `INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed) VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed)`,
-      {
-         $name: artist.name,
-         $dateOfBirth: artist.dateOfBirth,
-         $biography: artist.biography,
-         $isCurrentlyEmployed: artist.isCurrentlyEmployed,
-      },
-      (err, data) => {
-         err
-            ? res.status(400).send("Input field is not valid")
-            : res.status(201).json(data);
-      }
-   );
+   try {
+      // console.log(data);
+      await db.run(
+         `INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed) VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed)`,
+         {
+            $name: artist.name,
+            $dateOfBirth: artist.dateOfBirth,
+            $biography: artist.biography,
+            $isCurrentlyEmployed: artist.isCurrentlyEmployed,
+         },
+         (err, data) => {
+            err
+               ? res.status(400).send("Input field is not valid")
+               : res.status(201).json(data);
+         }
+      );
+   } catch (error) {
+      res.status(400).json(error);
+   }
+};
+
+const getArtistById = async (req, res) => {
+   const { id } = req.params;
+
+   try {
+      await db.get(
+         `SELECT * FROM Artist WHERE id = $id`,
+         {
+            $id: id,
+         },
+         (err, data) => {
+            err
+               ? res.status(404).send(`Artist with ID : ${id} 404 Not Found`)
+               : res.status(200).json(data);
+         }
+      );
+   } catch (error) {
+      res.status(500).json(error);
+   }
+};
+
+const updateArtist = async (req, res) => {
+   const { id } = req.params;
+   const { artist } = req.body;
+
+   try {
+      await db.run(
+         `UPDATE Artist SET name = "$name", date_of_birth = "$dateOfBirth",  = "$biography", is_currently_employed = "$isCurrentlyEmployed" WHERE id = "$id"`,
+         {
+            $name: artist.name,
+            $dateOfBirth: artist.dateOfBirth,
+            $biography: artist.biography,
+            $isCurrentlyEmployed: artist.isCurrentlyEmployed,
+            $id: id,
+         },
+         (err, data) => {
+            err
+               ? res.status(400).send("Update field")
+               : res.status(200).json(data);
+         }
+      );
+   } catch (error) {
+      res.status(400).json(error);
+   }
+};
+
+const deleteArtist = async (req, res) => {
+   const { id } = req.params;
+
+   try {
+      db.run(
+         `DELETE FROM Artist WHERE id = $id`,
+         {
+            $id: id,
+         },
+         (err, data) => {
+            err
+               ? res.status(404).send(`Artist with ID : ${id} not found`)
+               : res
+                    .status(200)
+                    .send(`Delete artist with ID : ${id} successfully !`);
+         }
+      );
+   } catch (error) {
+      res.status(404).json(error);
+   }
 };
 
 module.exports = {
    getAllArtists,
    createNewArtists,
+   updateArtist,
+   getArtistById,
+   deleteArtist,
 };
--- routes/artists.js
+++ routes/artists.js
@@ -5,9 +5,21 @@ const router = express.Router();
 const {
    getAllArtists,
    createNewArtists,
+   updateArtist,
+   getArtistById,
+   deleteArtist,
 } = require("../controllers/artists.js");
 
+const validateArtist = (req, res, next) => {
+   const { artist } = req.body;
+
+   return artist.isCurrentlyEmployed !== 0 ? next() : res.sendStatus(400);
+};
+
 router.get("/artists", getAllArtists);
-router.post("/artists", createNewArtists);
+router.post("/artists", validateArtist, createNewArtists);
+router.put("/artists/:id", updateArtist);
+router.get("/artists/:id", getArtistById);
+router.get("/artists/:id", deleteArtist);
 
 module.exports = router;
